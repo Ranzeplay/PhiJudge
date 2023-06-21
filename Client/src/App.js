97d534d05dabb53519722b9a108926b1c8fd668b
Jeb Feng <ranzeplay@outlook.com>
2023-06-21 20:26:37 +0800

    Add carbon-design-system and make it work

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { Button } from 'carbon-components-react';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+		<Button>Hello</Button>
       </header>
     </div>
   );
