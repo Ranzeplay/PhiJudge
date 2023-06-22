0ae04bd6b1d6a04e2e66be2853f6ee111e2c4b4f
Jeb Feng <ranzeplay@outlook.com>
2023-06-22 23:13:38 +0800

    Use React Component instead of function

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,28 +2,32 @@ import logo from './logo.svg';
 import './App.scss';
 
 import { Button } from 'carbon-components-react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
 
-        <Button>Hello</Button>
-      </div>
-    </div>
-  );
+                    <Button>Hello</Button>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
--- Client/src/NavBar.js
+++ Client/src/NavBar.js
@@ -2,29 +2,32 @@ import { Notification, User } from "@carbon/icons-react";
 import { Header, HeaderGlobalAction, HeaderGlobalBar, HeaderMenuItem, HeaderName, HeaderNavigation, SkipToContent } from "carbon-components-react";
 
 import './NavBar.scss'
+import React from "react";
 
-function NavBar() {
-    return (
-        <Header aria-label="navbar" className="NavBar">
-            <HeaderName href="/" prefix="Phi">Judge</HeaderName>
-            <SkipToContent />
+class NavBar extends React.Component {
+    render() {
+        return (
+            <Header aria-label="navbar" className="NavBar">
+                <HeaderName href="/" prefix="Phi">Judge</HeaderName>
+                <SkipToContent />
 
-            <HeaderNavigation>
-                <HeaderMenuItem href="/">Home</HeaderMenuItem>
-                <HeaderMenuItem href="#">Problem Set</HeaderMenuItem>
-                <HeaderMenuItem href="#">Submits</HeaderMenuItem>
-            </HeaderNavigation>
+                <HeaderNavigation>
+                    <HeaderMenuItem href="/">Home</HeaderMenuItem>
+                    <HeaderMenuItem href="#">Problem Set</HeaderMenuItem>
+                    <HeaderMenuItem href="#">Submits</HeaderMenuItem>
+                </HeaderNavigation>
 
-            <HeaderGlobalBar>
-                <HeaderGlobalAction aria-label="Notifications">
-                    <Notification size={20} />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction aria-label="User" href="/auth/signin">
-                    <User size={20} />
-                </HeaderGlobalAction>
-            </HeaderGlobalBar>
-        </Header>
-    )
+                <HeaderGlobalBar>
+                    <HeaderGlobalAction aria-label="Notifications">
+                        <Notification size={20} />
+                    </HeaderGlobalAction>
+                    <HeaderGlobalAction aria-label="User" href="/auth/signin">
+                        <User size={20} />
+                    </HeaderGlobalAction>
+                </HeaderGlobalBar>
+            </Header>
+        )
+    }
 }
 
 export default NavBar;
