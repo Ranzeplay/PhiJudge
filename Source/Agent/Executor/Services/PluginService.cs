5f7fed20cb5ed5b4d8bc7d115df8ec862229a7e0
Jeb Feng <ranzeplay@outlook.com>
2024-06-22 20:22:35 +0800

    Merge PluginPool into PluginService

--- Source/Agent/Executor/Services/PluginService.cs
+++ Source/Agent/Executor/Services/PluginService.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using PhiJudge.Agent.API.Plugin.Stages;
+using PhiJudge.Agent.API.Plugin;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,25 +13,73 @@ namespace PhiJudge.Agent.Executor.Services
 {
     internal class PluginService
     {
-        private readonly PluginPool PluginPool;
+        private readonly ILogger<PluginService> _logger;
 
-        public PluginService()
+        private Dictionary<string, Plugin> Plugins { get; } = [];
+
+        public PluginService(ILogger<PluginService> logger)
         {
-            PluginPool = new PluginPool();
+            _logger = logger;
 
-            PluginPool.InitPlugins();
-            PluginPool.LoadPlugins();
+            InitPlugins();
+            LoadPlugins();
         }
 
         public Plugin GetPlugin(string language)
         {
-            return PluginPool.Plugins.FirstOrDefault(p =>
+            return Plugins.FirstOrDefault(p =>
                 p.Value
                 .PluginEntrypoint
                 .SupportedLanguageId
                 .ToImmutableList()
                 .Contains(language)
             ).Value;
+        }
+
+        public void InitPlugins()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (var file in Directory.GetFiles(path, "*.dll"))
+            {
+                var assembly = Assembly.LoadFile(file);
+                var types = assembly.GetTypes();
+
+                if (types.First(t => t.GetInterface(nameof(IPluginEntrypoint)) is not null) is IPluginEntrypoint entrypoint &&
+                    types.First(t => t.GetInterface(nameof(ICompilationStage)) is not null) is ICompilationStage compilationStage &&
+                    types.First(t => t.GetInterface(nameof(IExecutionStage)) is not null) is IExecutionStage executionStage)
+                {
+                    Plugins.Add(entrypoint.Id, new Plugin(entrypoint, compilationStage, executionStage));
+                    _logger.LogInformation("Loaded plugin {0}, whose id is {1}", entrypoint.Name, entrypoint.Id);
+                }
+            }
+
+            _logger.LogInformation("Found {0} plugins", Plugins.Count);
+        }
+
+        public void LoadPlugins()
+        {
+            foreach (var plugin in Plugins.Values)
+            {
+                plugin.PluginEntrypoint.Load();
+            }
+
+            _logger.LogInformation("Loaded {0} plugins", Plugins.Count);
+        }
+
+        public void UnloadPlugins()
+        {
+            foreach (var plugin in Plugins.Values)
+            {
+                plugin.PluginEntrypoint.Unload();
+            }
+
+            _logger.LogInformation("Unloaded {0} plugins", Plugins.Count);
         }
     }
 }
