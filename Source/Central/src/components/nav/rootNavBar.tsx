c6189b959971eaeffda8ccdc6383d9276a91079e
Jeb Feng <ranzeplay@outlook.com>
2024-06-30 12:00:14 +0800

    Remove useless blur effect on navbar

--- Source/Central/src/components/nav/rootNavBar.tsx
+++ Source/Central/src/components/nav/rootNavBar.tsx
@@ -21,7 +21,7 @@ export function RootNavBar() {
 	}, []);
 
 	return (
-		<header className="sticky top-0 flex h-14 items-center gap-4 bg-background px-4 backdrop-blur-md shadow-md text-xs">
+		<header className="sticky top-0 flex h-14 items-center gap-4 bg-background px-4 shadow-md text-xs z-50">
 			<nav className="flex flex-row flex-grow gap-x-3 items-center">
 				<NavBarLink href="/" caption="PhiJudge" className="text-black mr-2 font-medium text-base font-serif" />
 				<NavBarLink href="/problems" caption="Problems" className="text-muted-foreground" />
