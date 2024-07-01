b1053c96f6cd94e353c3bb3d3372af1ca81fa56a
Jeb Feng <ranzeplay@outlook.com>
2024-07-01 23:17:54 +0800

    Update Central landing page

--- Source/Central/src/app/page.tsx
+++ Source/Central/src/app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <>
       <RootNavBar />
-      <div className='container prose mt-4'>
-        <h1>Hello, world</h1>
+      <div className='container flex flex-row items-center h-screen-without-nav'>
+        <div className='flex flex-col -translate-y-8 space-y-4'>
+          <h1 className='text-4xl font-bold font-serif'>PhiJudge</h1>
+          <p>An easy-to-use open judge system.</p>
+        </div>
       </div>
     </>
   );
--- Source/Central/tailwind.config.ts
+++ Source/Central/tailwind.config.ts
@@ -72,6 +72,9 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      height: {
+        'screen-without-nav': 'calc(100vh - 3.5rem)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
