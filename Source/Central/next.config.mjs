c3c10b39b7c60e4484b0bd7d12c5789e26f39725
Jeb Feng <ranzeplay@outlook.com>
2024-07-04 23:54:36 +0800

    wip : Update Docker configuration

--- Source/Central/next.config.mjs
+++ Source/Central/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  productionBrowserSourceMaps: false,
+};
 
 export default nextConfig;
