fd6d65b4990589662012f697a91c08bc14a0942d
Jeb Feng <ranzeplay@outlook.com>
2024-07-05 11:16:27 +0800

    Update environment variables guide

--- Source/Central/src/app/api/v0/auth/status/route.ts
+++ Source/Central/src/app/api/v0/auth/status/route.ts
@@ -1,35 +1,27 @@
 import { serverPrisma } from '@/lib/serverSidePrisma';
 import { createSupabaseServerSideClient } from '@/lib/supabase/server';
 import { NextResponse } from 'next/server';
-import { env } from 'process';
 
 export async function GET(_request: Request) {
-  if (env.DEBUG_ENABLE_AUTH) {
-    const supabase = createSupabaseServerSideClient();
-    const supabaseUser = await supabase.auth.getUser();
+  const supabase = createSupabaseServerSideClient();
+  const supabaseUser = await supabase.auth.getUser();
 
-    if (supabaseUser.error) {
-      return NextResponse.json({ isLoggedIn: false }, { status: 200 });
-    }
-
-    const prismaUser = await serverPrisma.user.findUnique({
-      where: {
-        id: supabaseUser.data!.user.id,
-      },
-    });
-
-    return NextResponse.json(
-      {
-        isLoggedIn: true,
-        userName: prismaUser?.userName,
-        isAdmin: prismaUser?.isAdmin,
-      },
-      { status: 200 }
-    );
-  } else {
-    return NextResponse.json(
-      { isLoggedIn: true, userName: 'debug' },
-      { status: 200 }
-    );
+  if (supabaseUser.error) {
+    return NextResponse.json({ isLoggedIn: false }, { status: 200 });
   }
+
+  const prismaUser = await serverPrisma.user.findUnique({
+    where: {
+      id: supabaseUser.data!.user.id,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      isLoggedIn: true,
+      userName: prismaUser?.userName,
+      isAdmin: prismaUser?.isAdmin,
+    },
+    { status: 200 }
+  );
 }
